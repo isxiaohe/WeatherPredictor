c8f68ebc55492830941dab74bf4f4be5e7a0712a
is_xiaohe <139753653+isxiaohe@users.noreply.github.com>
2024-06-30 01:20:38 +0800

    01:21

--- Weather-main004(1)/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Weather-main004_FILE C:/Users/24904/Desktop/qt/Weather-main004/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/Weather-main004.exe)
-set(__QT_DEPLOY_TARGET_Weather-main004_RUNTIME_DLLS C:/appli/qt_/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/appli/qt_/Qt/6.7.0/mingw_64/bin/Qt6Network.dll;C:/appli/qt_/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/appli/qt_/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
--- Weather-main004(1)/build/Qt_6_7_0_for_macOS_qt_qt6-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_Weather-main004_FILE /Users/matthew-xh/Study/CS/QT_WeatherPredictor/Weather-main004/build/Qt_6_7_0_for_macOS_qt_qt6-Debug/Weather-main004.app/Contents/MacOS/Weather-main004)
